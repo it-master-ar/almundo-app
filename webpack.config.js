9a55bdc69dc028ad28a4794e8885d8323423474a
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-02 11:05:38 -0300

    Adding webpack scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const isProd = process.env.ENV === 'prod'
+const filename = isProd
+   ? 'lib/app.min.js' : 'lib/app.js'
+
 module.exports = {
   entry: './src/app.js',
 
   output: {
     path: __dirname,
-    filename: 'lib/app.js'
+    filename
   },
 
   module: {
